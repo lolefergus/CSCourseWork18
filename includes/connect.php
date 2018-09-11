da9332f218d73a29391bf7752a6673e9eb028c23
Fergus <fergus.lole@outlook.com>
2018-09-11 15:42:16 +0100

    changed database connection

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,11 @@
 <?php
 ini_set('display_errors', 'On');
   error_reporting(E_ALL | E_STRICT);
-  
-$servername = "cp1692.netcetera.co.uk";
-$username = "fergus_user";
-$password = "kVTADRjXwCjB3kuLa7";
-$dbname = "fergus_db";
+
+$servername = "compscicoursework.database.windows.net";
+$username = "fergusLole";
+$password = "m1Cr020ft";
+$dbname = "CompSciCourseWork2018";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn)
