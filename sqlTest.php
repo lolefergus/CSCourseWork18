86a072727f09458c54dea49a1f0af04cd7dd42b0
Fergus <fergus.lole@outlook.com>
2018-12-18 15:14:37 +0000

    removes need for problem code section

--- sqlTest.php
+++ sqlTest.php
@@ -1,28 +1,28 @@
 <html>
-<head>
-  <title>SQL Statment Test</title>
-</head>
+  <head>
+    <title>SQL Statment Test</title>
+  </head>
 
-<body>
-  <header>SQL Output:</header>
-  <?php
+  <body>
+    <header>SQL Output:</header>
+    <?php
 
-  //SQL Query
-  $Query = sqlsrv_query ($conn, "Select * from accounts;");
+    //SQL Query
+    $Query = sqlsrv_query ($conn, "Select * from accounts;");
 
-  //If fails
-  // if ($Query == false)
-  // {
-  //   //displays error message
-  //   print "SQL Error: ", sqlsrv_errors (SQLSRV_ERR_ALL);
-  // }
-  // else
-  // {
-  //   //displays Query
-  //   print $Query;
-  // }
+    //If fails
+    // if ($Query == false)
+    // {
+    //   //displays error message
+    //   print "SQL Error: ", sqlsrv_errors (SQLSRV_ERR_ALL);
+    // }
+    // else
+    // {
+    //   //displays Query
+    //   print $Query;
+    // }
 
-
-  ?>
-</body>
+    print $Query;
+    ?>
+  </body>
 </html>
