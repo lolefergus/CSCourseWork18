edba7029da1780f2a6af0f0c1b79b6b0ca93b623
Fergus <fergus.lole@outlook.com>
2018-12-18 14:46:02 +0000

    Un atempt attempt at de-fucking

--- includes/connect.php
+++ includes/connect.php
@@ -15,20 +15,13 @@ $password = "m1Cr020ft";
 
 // PHP Data Objects(PDO) Sample Code:
 // PHP Data Objects(PDO) Sample Code:
-// try {
-//     $conn = new PDO("sqlsrv:server = tcp:compscicoursework.database.windows.net,1433; Database = CompSciCourseWork2019", "ferguslole", $password);
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// }
-// catch (PDOException $e) {
-//     print("Error connecting to SQL Server.");
-//     die(print_r($e));
-// }
-$conn = new PDO("sqlsrv:server = tcp:compscicoursework.database.windows.net,1433; Database = CompSciCourseWork2019", "ferguslole", $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-if ($conn = true){
-  print("Error connecting to SQL Server.");
-  die(print_r($e));
+try {
+    $conn = new PDO("sqlsrv:server = tcp:compscicoursework.database.windows.net,1433; Database = CompSciCourseWork2019", "ferguslole", $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
 // SQL Server Extension Sample Code:
