580f60e8a16b6a6b5e2dac6a5d47ac5121cf19ba
Fergus <fergus.lole@outlook.com>
2019-01-07 12:28:46 +0000

    removed place holder text

--- sqlTest.php
+++ sqlTest.php
@@ -63,7 +63,7 @@ include($root.'/includes/connect.php');
       <div class="col-12">
           <div class="form-group has-floating-label">
               <label class="control-label">Email</label>
-              <input type="text" class="form-control form-control-lg" placeholder="email">
+              <input type="text" class="form-control form-control-lg" placeholder="">
               <span class="bar"></span>
             </div>
           </div>
@@ -73,7 +73,7 @@ include($root.'/includes/connect.php');
           <div class="col-12">
             <div class="form-group has-floating-label">
               <label class="control-label">Password</label>
-              <input type="password" class="form-control form-control-lg" placeholder="pWord">
+              <input type="password" class="form-control form-control-lg" placeholder="">
               <span class="bar"></span>
             </div>
           </div>
