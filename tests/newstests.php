9ec937325bfca1378154d386d1a4eeabdf67ef98
Fergus <fergus.lole@outlook.com>
2019-01-20 21:00:07 +0000

    that part worked, testing next stage

--- tests/newstests.php
+++ tests/newstests.php
@@ -15,35 +15,21 @@ include($root.'/includes/connect.php');
 
     $id = 7;
     $Query = sqlsrv_query($conn,"SELECT * FROM news WHERE id = $id");
-    print $Query;
-    echo $Query['id'];
-    echo $Query['title'];
-    echo $Query['authorId'];
-    echo $Query['body'];
-    echo $Query['image'];
-
-    echo '<p>??</p>';
 
     $row = sqlsrv_fetch_array($Query);
-    echo $row['id'];
-    echo $row['title'];
-    echo $row['authorId'];
-    echo $row['body'];
-    echo $row['image'];
 
-    echo 'EEE';
-    // while ($row = sqlsrv_fetch_array($Query))
-    // {
-    //   $title = $row['title'];
-    //   $body = $row['body'];
-    //   $image = $row['image'];
-    //
-    //   print "It be getting the far";
-    //   print $id;
-    //   print $title;
-    //   print $body;
-    //   print $image;
-    // }
+    while ($row = sqlsrv_fetch_array($Query))
+    {
+      $title = $row['title'];
+      $body = $row['body'];
+      $image = $row['image'];
+
+      print "It be getting the far";
+      print $id;
+      print $title;
+      print $body;
+      print $image;
+    }
     ?>
 
   </body>
