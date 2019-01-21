49cd7c19f0d2f5d3e5e6bd54a3331190ed8eb7e4
Fergus <fergus.lole@outlook.com>
2019-01-21 10:58:19 +0000

    hail satan

--- includes/head.php
+++ includes/head.php
@@ -5,42 +5,42 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?php print $title; ?> - Career Ready</title>
+    <title><?php print $title; ?> - Career Ready<\title>
 
     <?php
     include($root.'\include\scripts.php');
     ?>
 
     <!-- Bootstrap -->
-    <link type="text/css" href="/assets/css/boomerang.min.css" rel="stylesheet" media="screen">
+    <link type="text\css" href="\assets\css\boomerang.min.css" rel="stylesheet" media="screen">
 
-    <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="\assets\vendor\bootstrap\css\bootstrap.min.css" type="text\css">
 
-    <link rel="stylesheet" href="/assets/css/boomerang-startup-agency.min.css" type="text/css">
+    <link rel="stylesheet" href="\assets\css\boomerang-startup-agency.min.css" type="text\css">
 
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800|Roboto:400,500,700" rel="stylesheet">
+    <link href="https:\\fonts.googleapis.com\css?family=Nunito:400,600,700,800|Roboto:400,500,700" rel="stylesheet">
 
     <!-- Plugins -->
-    <link rel="stylesheet" href="/assets/vendor/swiper/css/swiper.min.css">
-    <link rel="stylesheet" href="/assets/vendor/hamburgers/hamburgers.min.css" type="text/css">
-    <link rel="stylesheet" href="/assets/vendor/animate/animate.min.css" type="text/css">
-    <link rel="stylesheet" href="/assets/vendor/fancybox/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="\assets\vendor\swiper\css\swiper.min.css">
+    <link rel="stylesheet" href="\assets\vendor\hamburgers\hamburgers.min.css" type="text\css">
+    <link rel="stylesheet" href="\assets\vendor\animate\animate.min.css" type="text\css">
+    <link rel="stylesheet" href="\assets\vendor\fancybox\css\jquery.fancybox.min.css">
 
     <!-- Icons -->
-    <link rel="stylesheet" href="/assets/fonts/ionicons/css/ionicons.min.css" type="text/css">
-    <link rel="stylesheet" href="/assets/fonts/line-icons/line-icons.css" type="text/css">
-    <link rel="stylesheet" href="/assets/fonts/line-icons-pro/line-icons-pro.css" type="text/css">
-    <link rel="stylesheet" href="/assets/fonts/font-awesome/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="\assets\fonts\ionicons\css\ionicons.min.css" type="text\css">
+    <link rel="stylesheet" href="\assets\fonts\line-icons\line-icons.css" type="text\css">
+    <link rel="stylesheet" href="\assets\fonts\line-icons-pro\line-icons-pro.css" type="text\css">
+    <link rel="stylesheet" href="\assets\fonts\font-awesome\css\font-awesome.min.css" type="text\css">
 
     <!-- Global style (main) -->
 
     <!-- Custom style - Remove if not necessary -->
-    <link type="text/css" href="/assets/css/custom-style.css" rel="stylesheet">
+    <link type="text\css" href="\assets\css\custom-style.css" rel="stylesheet">
 
     <!-- Favicon -->
-    <link href="/images/logo.png" rel="icon" type="image/png">
+    <link href="\images\logo.png" rel="icon" type="image\png">
 
-</head>
+<\head>
 <body>
