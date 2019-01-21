a09a95d7e0a63ec2ead78b610d9d2d988012c773
Fergus <fergus.lole@outlook.com>
2019-01-21 10:50:18 +0000

    removed unneeded code

--- includes/connect.php
+++ includes/connect.php
@@ -2,21 +2,12 @@
 ini_set('display_errors', 'On');
   error_reporting(E_ALL | E_STRICT);
 
-//$servername = "compscicoursework.database.windows.net";
-//$username = "fergusLole";
+$userName = "fergusLole";
 $password = "m1Cr020ft";
-//$dbname = "CompSciCourseWork2018";
+$dbname = "CompSciCourseWork2019";
 
-//$conn = mysqli_connect($servername, $username, $password, $dbname, 1433);
-//if (!$conn)
-//{
-  //die("Connection failed: " . mysqli_connect_error() . " And: " . mysqli_connect_errno());
-//}
-
-// PHP Data Objects(PDO) Sample Code:
-// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:compscicoursework.database.windows.net,1433; Database = CompSciCourseWork2019", "ferguslole", $password);
+    $conn = new PDO("sqlsrv:server = tcp:compscicoursework.database.windows.net,1433; Database = CompSciCourseWork2019", $userName, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -25,7 +16,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ferguslole@compscicoursework", "pwd" => $password, "Database" => "CompSciCourseWork2019", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "ferguslole@compscicoursework", "pwd" => $password, "Database" => $dbname, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:compscicoursework.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
