a36323422744071a2e2c84d4158d80abc7ac7466
Fergus <fergus.lole@outlook.com>
2019-01-21 22:04:46 +0000

    Made easier to change address

--- includes/connect.php
+++ includes/connect.php
@@ -2,24 +2,23 @@
 ini_set('display_errors', 'On');
   error_reporting(E_ALL | E_STRICT);
 
-$userName = "fergusLole";
-$password = "m1Cr020ft";
-$dbname = "CompSciCourseWork2019";
+$userName = "fergusLole"; //username of account to access Database
+$dbDomain = "@compscicoursework"; //domain used as extension of sign in
+$password = "m1Cr020ft"; //DB Password
+$dbName = "CompSciCourseWork2019"; //DB name
+$dbAddress = "tcp:compscicoursework.database.windows.net,1433";//DB adress protcol,adress,port
 
 try {
-    $conn = new PDO("sqlsrv:server = tcp:compscicoursework.database.windows.net,1433; Database = CompSciCourseWork2019", $userName, $password);
+    $conn = new PDO("sqlsrv:server = ". $dbAddress ."; Database = ". $dbName, $userName, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
-}
+
 
 //Generates conection string:
-$connectionInfo = array("UID" => "ferguslole@compscicoursework", "pwd" => $password, "Database" => $dbname, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:compscicoursework.database.windows.net,1433";
+$connectionInfo = array("UID" => $userName . $dbDomain, "pwd" => $password, "Database" => $dbName, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = $dbAddress;
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-//Console.print $conn;
-
 ?>
