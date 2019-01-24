612e8280c3ef34b295b8098faee04e046d12b5d3
Fergus <fergus.lole@outlook.com>
2019-01-24 12:12:02 +0000

    made invalid

--- tests/regex.php
+++ tests/regex.php
@@ -1,6 +1,6 @@
 <?php
-$email = "ferguslole@online.sch.im";
-if (preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
+$email = "fergu..slole@online.sch.im";
+if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
   echo "Valid";
 }
 else {
