ad4f152d971542a14cfce91c01528d13997631c4
Fergus <fergus.lole@outlook.com>
2019-01-24 21:57:19 +0000

    b zoh[inkcg; f'omkjnn b/m.lhluibnvl

--- tests/regex.php
+++ tests/regex.php
@@ -1,10 +1,10 @@
 <?php
 ini_set('display_errors', 'On');
 
-// $root = $_SERVER['DOCUMENT_ROOT'];
-// $title = "Login";
-// include($root.'/includes/head.php');
-// include($root.'/includes/connect.php');
+$root = $_SERVER['DOCUMENT_ROOT'];
+$title = "Login";
+include($root.'/includes/head.php');
+include($root.'/includes/connect.php');
 
 $email = "ferguslole@online.sch.im";
 if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
