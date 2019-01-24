f114f79ab62056960792340b35f57ffa0360b9b7
Fergus <fergus.lole@outlook.com>
2019-01-24 11:54:23 +0000

    changed outputs

--- tests/regex.php
+++ tests/regex.php
@@ -1,3 +1,8 @@
 <?php
-print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", "fergus.lole@gmail.com");
+if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", "fergus.lole@gmail.com")) {
+  echo "Valid";
+}
+else {
+  echo "Invalid";
+}
 ?>
