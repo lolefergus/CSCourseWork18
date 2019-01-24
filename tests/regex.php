06615d0f48e9dc8a35292dd149045b9d49ff999c
Fergus <fergus.lole@outlook.com>
2019-01-24 12:09:03 +0000

    changed logic

--- tests/regex.php
+++ tests/regex.php
@@ -1,5 +1,6 @@
 <?php
-if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", "fergus.lole@gmail.com")) {
+$email = "ferguslole@online.sch.im"
+if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
   echo "Valid";
 }
 else {
