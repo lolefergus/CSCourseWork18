ed345a5c5d2ad484fb14da7a3ae9637d9cf5957f
Fergus <fergus.lole@outlook.com>
2019-01-24 12:11:09 +0000

    removed print

--- tests/regex.php
+++ tests/regex.php
@@ -1,6 +1,6 @@
 <?php
 $email = "ferguslole@online.sch.im";
-if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
+if (preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
   echo "Valid";
 }
 else {
