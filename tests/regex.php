fedc32eec37fb204201a3a984371c70d86005b87
Fergus <fergus.lole@outlook.com>
2019-01-24 12:13:36 +0000

    made valid

--- tests/regex.php
+++ tests/regex.php
@@ -1,5 +1,5 @@
 <?php
-$email = "fergu..slole@online.sch.im";
+$email = "ferguslole@online.sch.im";
 if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
   echo "Valid";
 }
