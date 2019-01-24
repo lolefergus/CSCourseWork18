2868ed622b34273b994524ee60a6ba1d26a9e704
Fergus <fergus.lole@outlook.com>
2019-01-24 21:11:39 +0000

    added includes

--- tests/regex.php
+++ tests/regex.php
@@ -1,4 +1,9 @@
 <?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+$title = "Login";
+include($root.'/includes/head.php');
+include($root.'/includes/connect.php');
+
 $email = "ferguslole@online.sch.im";
 if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
   echo "Valid";
