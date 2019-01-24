d7b08db18ce8b5b145642266997174471daba172
Fergus <fergus.lole@outlook.com>
2019-01-24 21:27:00 +0000

    chasing error to includes

--- tests/regex.php
+++ tests/regex.php
@@ -2,7 +2,7 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 $title = "Login";
 include($root.'/includes/head.php');
-include($root.'/includes/connect.php');
+// include($root.'/includes/connect.php');
 
 $email = "ferguslole@online.sch.im";
 if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
