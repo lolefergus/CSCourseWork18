89dc80bb3e06dc1e17a6bc2d0f747b9164abb2ad
Fergus <fergus.lole@outlook.com>
2019-01-30 21:51:47 +0000

    changed spacing

--- tests/login.php
+++ tests/login.php
@@ -10,35 +10,43 @@ $password = "qwerty";
 //sets values from info entered on page
 // $email = $_REQUEST[$_POST['email']];
 $escapedEmail = preg_quote ($email);
-if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email)) {
+if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $email))
+{
   // $password = $_REQUEST[$_POST['password']];
   //searches for matching users
   $search = sqlsrv_query($conn, "SELECT [id],[saltedPassword] FROM [dbo].[accounts] WHERE [email] = '$email'");
   //Check num result found, then if only one
   $count = sqlsrv_num_rows($search);
-  if(1 == $count){
+  if(1 == $count)
+  {
     print "found email match"; //ROMOVE
-      //gets hashed password from DB
-      while($row = sqlsrv_fetch_array($search)){
-        $hashed = $row['saltedPassword'];
-        $id = $row['id'];
-        //Use password_verify to check unhashed password is same as hashed password
-        if (password_verify($password, $encrypted)) {
-          print "Succesful"; //REMOVE
-          //Start a session
-          session_start();
-          //Use user's id to identify the session
-          $_SESSION['id']=$id;
-          $_SESSION['last_activity'] = time(); //your last activity was now, having logged in.
-          //sends user to account homepage
-          // header('location: /account/'); UNCOMENT AFTER DEBUGGING
-          } else {
-            $message = 'There was an error with your details.';
-          }
-        }
-      } else {
-        $message = 'There was an error with your details.';
+    //gets hashed password from DB
+    while($row = sqlsrv_fetch_array($search))
+    {
+      $hashed = $row['saltedPassword'];
+      $id = $row['id'];
+      //Use password_verify to check unhashed password is same as hashed password
+      if (password_verify($password, $encrypted))
+      {
+        print "Succesful"; //REMOVE
+        //Start a session
+        session_start();
+        //Use user's id to identify the session
+        $_SESSION['id']=$id;
+        $_SESSION['last_activity'] = time(); //your last activity was now, having logged in.
+        //sends user to account homepage
+        // header('location: /account/'); UNCOMENT AFTER DEBUGGING
       }
+      else
+      {
+          $message = 'There was an error with your details.';
+      }
+    }
+  }
+  else
+  {
+    $message = 'There was an error with your details.';
+  }
 }
 else {
     print'email invalid';
