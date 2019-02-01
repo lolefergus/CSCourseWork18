24e74a5244b1e36dcf5d78e1d9414cf744cf7567
Fergus <fergus.lole@outlook.com>
2019-02-01 11:02:07 +0000

    chnaged logic

--- tests/login.php
+++ tests/login.php
@@ -17,7 +17,7 @@ if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[
   $search = sqlsrv_query($conn, "SELECT TOP (3) * FROM news");
   //Check num result found, then if only one
   $count = sqlsrv_num_rows($search);
-  if(1 == $count)
+  if(3 == $count)
   {
     print "found email match"; //ROMOVE
     //gets hashed password from DB
