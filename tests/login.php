44a289377ecce9a82621046b990b29d297f60987
Fergus <fergus.lole@outlook.com>
2019-02-07 11:17:13 +0000

    chnaged paramater logic

--- tests/login.php
+++ tests/login.php
@@ -13,7 +13,8 @@ $escapedEmail = preg_quote ($email);
 if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $escapedEmail))
 {
   //searches for matching users
-  $Query = sqlsrv_query($conn, "SELECT * FROM accounts WHERE email = '$email'", array(&$_POST['query']), array(SQLSRV_CURSOR_STATIC));
+  $params = array();
+  $Query = sqlsrv_query($conn, "SELECT * FROM accounts WHERE email = '$email'", $params, array(SQLSRV_CURSOR_STATIC));
   print "<p></p>". $Query . "<p></p>";
   //Check num result found, then if only one
   $count = sqlsrv_num_rows($Query);
