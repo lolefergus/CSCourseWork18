0a4c11f476f24f07f6d5fa4b2c81242781ee0146
Fergus <fergus.lole@outlook.com>
2019-02-07 18:28:22 +0000

    migrated working code - almost certainly not working now

--- account/login/index.php
+++ account/login/index.php
@@ -1,49 +1,71 @@
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+$title = "Login";
+include($root.'/includes/connect.php');
+
+if(isset($_POST['login']))
+{
+//sets values from info entered on page
+$email = $_REQUEST[$_POST['email']];
+$escapedEmail = preg_quote ($email);
+if (print preg_match( "[a-zA-Z0-9_%\+-]+(\.[a-zA-Z0-9_%\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+", $escapedEmail))
+{
+  //searches for matching users
+  $Query = sqlsrv_query($conn, "SELECT * FROM accounts WHERE email = '$email'", array(), array('Scrollable' => 'buffered'));
+  print "<p></p>". $Query . "<p></p>";
+  //Check num result found, then if only one
+  $count = sqlsrv_num_rows($Query);
+  print $count;
+  if(1 == $count) //checks only one match
+  {
+    print "found email match"; //ROMOVE
+    //gets hashed password from DB
+    while($row = sqlsrv_fetch_array($Query))
+    {
+      $hashed = $row['saltedPassword'];
+      $id = $row['id'];
+      //Use password_verify to check unhashed password is same as hashed password
+      $password = $_REQUEST[$_POST['password']];
+      if (password_verify($password, $hashed))
+      {
+        print "Succesful"; //REMOVE
+        //Start a session
+        if (session_status() !== PHP_SESSION_ACTIVE)
+        {
+          print "Session started";
+          session_start();
+          //Use user's id to identify the session
+          $_SESSION['id']=$id;
+          $_SESSION['last_activity'] = time(); //your last activity was now, having logged in.
+          //sends user to account homepage
+          // header('location: /account/'); UNCOMENT AFTER DEBUGGING
+        }
+        else
+        {
+         print "Already signed in";
+        }
+      }
+      else
+      {
+          $message = 'There was an error with your details.';
+      }
+    }
+  }
+  else
+  {
+    print 'There was an error with your details.';
+  }
+}
+else
+{
+  print'email invalid';
+}
+}
+
+include($root.'/includes/head.php');
+?>
 <!DOCTYPE html>
 <html>
-<?php
-    $root = $_SERVER['DOCUMENT_ROOT'];
-    $title = "Login";
-    include($root.'/includes/head.php');
-    include($root.'/includes/connect.php');
-
-    if(isset($_POST['login'])){
-      echo "this bit works";
-      //sets values from info entered on page
-      $email = $_REQUEST[$_POST['email']];
-      if (preg_match('\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b#i', $email)=== 1) {
-        print'email match';//remove after tesing
-        $password = $_REQUEST[$_POST['password']];
-          print$password;//REMOVE
-        //searches for matching users
-        $search = sqlsrv_query($conn, "SELECT * FROM accounts WHERE email='$email'");
-        //Check num result found, then if only one
-        $count = sqlsrv_num_rows($search);
-        if(1 == $count){
-          //gets hashed password from DB
-          while($row = sqlsrv_fetch_array($search)){
-            $hashed = $row['password'];
-            $id = $row['id'];
-            //Use password_verify to check unhashed password is same as hashed password
-            if (password_verify($password, $encrypted)) {
-              //Start a session
-              session_start();
-              //Use user's id to identify the session
-              $_SESSION['id']=$id;
-              $_SESSION['last_activity'] = time(); //your last activity was now, having logged in.
-              //sends user to account homepage
-              // header('location: /account/'); UNCOMENT AFTER DEBUGGING
-              } else {
-                $message = 'There was an error with your details.';
-              }
-            }
-          } else {
-            $message = 'There was an error with your details.';
-          }
-      }
-      print'email invalid';
-    }
-
-?>
 <body>
 
   <main class="body-wrap">
