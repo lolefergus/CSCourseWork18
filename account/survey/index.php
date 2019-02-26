b401cda992cc77f5f7fd73a92c9f862ac4ca3362
Fergus <fergus.lole@outlook.com>
2019-02-26 09:56:46 +0000

    hard set Id and fixed querys

--- account/survey/index.php
+++ account/survey/index.php
@@ -7,17 +7,19 @@
     include($root.'/includes/connect.php');
 ?>
 <?php
-//check account type once session set up
+//check account type and id once session set up
+//in mean time set id manualy here
+$id = 1;
 
 //checks how many times survey been taken
-if (sqlsrv_has_rows (sqlsrv_query($conn, "SELECT answer FROM skillSurveyAs WHERE studentId = id AND surveyNo = 3")))
+if (sqlsrv_has_rows (sqlsrv_query($conn, "SELECT answer FROM skillSurveyAs WHERE studentId = $id AND surveyNo = 3")))
 {
   $serveyNum = 4;
 }
-else if (sqlsrv_has_rows (sqlsrv_query($conn, "SELECT answer FROM skillSurveyAs WHERE studentId = id AND surveyNo = 2"))) {
+else if (sqlsrv_has_rows (sqlsrv_query($conn, "SELECT answer FROM skillSurveyAs WHERE studentId = $id AND surveyNo = 2"))) {
   $serveyNum = 3;
 }
-else if (sqlsrv_has_rows (sqlsrv_query($conn, "SELECT answer FROM skillSurveyAs WHERE studentId = id AND surveyNo = 1"))) {
+else if (sqlsrv_has_rows (sqlsrv_query($conn, "SELECT answer FROM skillSurveyAs WHERE studentId = $id AND surveyNo = 1"))) {
   $serveyNum = 2;
 }
 else {
