7c5e58187f01b898c8781acce18161272244eeb1
Fergus <fergus.lole@outlook.com>
2019-02-28 11:04:31 +0000

    chnaged layout

--- account/survey/questions.php
+++ account/survey/questions.php
@@ -3,68 +3,62 @@
   <div class="slice sct-color-1" style="padding-top: 20px; padding-bottom: 20px;">
     <div class="container container-lg">
       <div class="row masonry cols-xs-space cols-sm-space cols-md-space" style="position: relative;">
-        <?php
-        //as section is a foreign key dynamicly asigned when creating a section in the surveySection table it may not go up as 1,2,3
-        //therfore I must first check which numbers are used as section IDs
-        $NumSectionsQuery = sqlsrv_query($conn, "SELECT DISTINCT section FROM skillSurveyQs"); //selects one of each differnt value in section column of table
-        while ($currentSection = sqlsrv_fetch_array($NumSectionsQuery)) //loops for each section
-        {
-          //reads currecnt section
-          $section = $currentSection['section'];
-          $Query = sqlsrv_query($conn, "SELECT qid, question FROM skillSurveyQs WHERE section = $section"); //selects questions
-          $headingQuery = sqlsrv_query($conn, "SELECT title, description FROM surveySection WHERE sectionId = $section"); //selects tile info
-          $headerInfo = sqlsrv_fetch_array($headingQuery);
-          $title = $headerInfo['title']; //defines title of current section from query results
-          $description = $headerInfo['description']; //as above but for section description
-
-          echo'
-          <div>
+        <div>
           <table class="table table-hover">
-            <tbody>
-              <tr>
-                <th scope="col">'.$title.'</th>
-              </tr>
-              <tr>
-                <th scope="col">'.$description.'</th>
-              </tr>
-            </tbody>
+            <?php
+            //as section is a foreign key dynamicly asigned when creating a section in the surveySection table it may not go up as 1,2,3
+            //therfore I must first check which numbers are used as section IDs
+            $NumSectionsQuery = sqlsrv_query($conn, "SELECT DISTINCT section FROM skillSurveyQs"); //selects one of each differnt value in section column of table
+            while ($currentSection = sqlsrv_fetch_array($NumSectionsQuery)) //loops for each section
+            {
+              //reads currecnt section
+              $section = $currentSection['section'];
+              $Query = sqlsrv_query($conn, "SELECT qid, question FROM skillSurveyQs WHERE section = $section"); //selects questions
+              $headingQuery = sqlsrv_query($conn, "SELECT title, description FROM surveySection WHERE sectionId = $section"); //selects tile info
+              $headerInfo = sqlsrv_fetch_array($headingQuery);
+              $title = $headerInfo['title']; //defines title of current section from query results
+              $description = $headerInfo['description']; //as above but for section description
 
-            <tbody>
-
-          ';
-          while ($row = sqlsrv_fetch_array($Query)) //loops for each question withing section
-          {
-            $qid = $row['qid'];
-            $question = $row['question'];
-            echo'
-            <tr>
-              <td scope="row">' . $question . '</td>
-              <td scope="row" hidden>'. $id . '</td>
-              <td scope="row">
-
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                  <label class="btn btn-secondary active">
-                    <input type="radio" name="1" id="option1" autocomplete="off" style="backgroundcolor:#599A90">
-                  </label>
-                  <label class="btn btn-secondary">
-                    <input type="radio" name="2" id="option2" autocomplete="off" style="backgroundcolor:#CB5F2B">
-                  </label>
-                  <label class="btn btn-secondary">
-                    <input type="radio" name="3" id="option3" autocomplete="off" style="backgroundcolor:#9E264B">
-                  </label>
-                </div>
-
-              </td>
-            </tr>
-            ';
-          }
-          echo'
-            </tbody>
+              echo'
+              <tbody>
+                <tr>
+                  <th scope="col">'.$title.'</th>
+                </tr>
+                <tr>
+                  <th scope="col">'.$description.'</th>
+                </tr>
+              ';
+              while ($row = sqlsrv_fetch_array($Query)) //loops for each question withing section
+              {
+                $qid = $row['qid'];
+                $question = $row['question'];
+                echo'
+                <tr>
+                  <td scope="row">' . $question . '</td>
+                  <td scope="row" hidden>'. $id . '</td>
+                  <td scope="row">
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                      <label class="btn btn-secondary active">
+                        <input type="radio" name="1" id="option1" autocomplete="off" style="backgroundcolor:#599A90">
+                      </label>
+                      <label class="btn btn-secondary">
+                        <input type="radio" name="2" id="option2" autocomplete="off" style="backgroundcolor:#CB5F2B">
+                      </label>
+                      <label class="btn btn-secondary">
+                        <input type="radio" name="3" id="option3" autocomplete="off" style="backgroundcolor:#9E264B">
+                      </label>
+                    </div>
+                  </td>
+                </tr>
+                ';
+              }
+              echo'
+              </tbody>
+              ';
+            }
+            ?>
           </table>
-          </div>
-          ';
-        }
-        ?>
+        </div>
       </div>
     </div>
 
