e99d47fc98d9ddf2f731dc2b87aee526f0b3a514
Fergus <fergus.lole@outlook.com>
2019-03-04 10:40:59 +0000

    changed transfer method

--- account/create/details/index.php
+++ account/create/details/index.php
@@ -15,7 +15,7 @@ include($root.'/includes/connect.php');
 
 
     <?php
-    $accountType = $_POST['accountType'];
+
 
     if (isset($_POST['create']))
     {
@@ -26,132 +26,138 @@ include($root.'/includes/connect.php');
       $region = $_POST['region'];
       $workOrSchool = $_POST['workOrSchool'];
       $joinYear = (string)date("Y");
+      $accountType = $_POST['accountType'];
 
       sqlsrv_query($conn, "INSERT INTO accounts (firstName, lastName, password, email, accountType, joinYear, region, workOrSchool) values ($firstName, $lastName, $password, $email, $accountType, $joinYear, $region, $workOrSchool) ");
       echo '<script>window.location.href="/admin/news/index.php";</script>';
     }
 
-    ?>
+    else
+    {
+      $accountType = $_GET['type'];
+      ?>
 
-    <container>
+      <container>
 
-      <section class="slice sct-color-1">
-        <div class="container container-lg">
-          <div class="row align-items-center cols-xs-space cols-sm-space cols-md-space">
-            <div class="col-lg-6">
-              <form class="form-default form-material">
+        <section class="slice sct-color-1">
+          <div class="container container-lg">
+            <div class="row align-items-center cols-xs-space cols-sm-space cols-md-space">
+              <div class="col-lg-6">
+                <form class="form-default form-material">
 
-                <div class="row col-12">
-                  <h3>Register for the Program</h3>
-                </div>
+                  <div class="row col-12">
+                    <h3>Register for the Program</h3>
+                  </div>
 
-                <div class="row">
-                  <div class="col-md-6 ml-lg-auto">
-                    <div class="form-group has-floating-label">
-                      <label class="control-label">Forename</label>
-                      <input type="text" class="form-control form-control-lg" placeholder="">
-                      <span class="bar"></span>
+                  <div class="row">
+                    <div class="col-md-6 ml-lg-auto">
+                      <div class="form-group has-floating-label">
+                        <label class="control-label">Forename</label>
+                        <input type="text" class="form-control form-control-lg" placeholder="">
+                        <span class="bar"></span>
+                      </div>
+                    </div>
+
+                    <div class="col-md-6 ml-lg-auto">
+                      <div class="form-group has-floating-label">
+                        <label class="control-label">Surname</label>
+                        <input type="text" class="form-control form-control-lg" placeholder="">
+                        <span class="bar"></span>
+                      </div>
                     </div>
                   </div>
 
-                  <div class="col-md-6 ml-lg-auto">
-                    <div class="form-group has-floating-label">
-                      <label class="control-label">Surname</label>
-                      <input type="text" class="form-control form-control-lg" placeholder="">
-                      <span class="bar"></span>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-12">
-                    <div class="form-group has-floating-label">
-                      <label class="control-label">Email</label>
-                      <input type="text" class="form-control form-control-lg" placeholder="">
-                      <span class="bar"></span>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-12">
-                    <div class="form-group has-floating-label">
-                      <label class="control-label">Password</label>
-                      <input type="password" class="form-control form-control-lg" placeholder="">
-                      <span class="bar"></span>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="form-group">
-                  <?php if ($accountType == "Student") { //changes question for student or mentor
-                    echo '<label>Select the Region of the Island You Live In:</label>';
-                  }
-                  else {
-                    echo '<label>Select the Region of the Island You Work In:</label>';
-                  } ?>
-                  <select class="form-control selectpicker select2-hidden-accessible" data-minimum-results-for-search="Infinity" tabindex="-1" aria-hidden="true">
-                    <option value="North">North</option>
-                    <option value="Center">Centre</option>
-                    <option value="South">South</option>
-                  </select>
-                </div>
-
-                <?php if ($accountType == "Student") { //changes question for student or mentor
-                  echo '
-                  <div class="form-group">
-                    <label class="control-label">Enter the Name of the School You Attend</label>
-                    <select class="form-control selectpicker select2-hidden-accessible" data-minimum-results-for-search="Infinity" tabindex="-1" aria-hidden="true">
-                      <option value="RGS">Ramsey Grammar School</option>
-                      <option value="CRHS">Castle Rushen High School</option>
-                      <option value="QEII">Queen Elizabeth II High School</option>
-                      <option value="BHS">Ballakermeen High School</option>
-                      <option value="SNHS">St Ninians High School</option>
-                      <option value="UCM">University College Isle of Man</option>
-                    </select>
-                  </div>';
-                }
-                else {
-                  echo '
                   <div class="row">
                     <div class="col-12">
                       <div class="form-group has-floating-label">
-                        <label class="control-label">Enter the Name of the Company You Work For</label>
+                        <label class="control-label">Email</label>
+                        <input type="text" class="form-control form-control-lg" placeholder="">
+                        <span class="bar"></span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-12">
+                      <div class="form-group has-floating-label">
+                        <label class="control-label">Password</label>
                         <input type="password" class="form-control form-control-lg" placeholder="">
                         <span class="bar"></span>
                       </div>
                     </div>
-                  </div>';
-                } ?>
-
-
-                <div class="row cols-xs-space cols-sm-space cols-md-space align-items-center text-left">
-
-                  <div class="col-lg-3 col-md-4">
-                    <div class="mt-4">
-                      <input type="hidden" name="accountType" value="<?php echo $accountType?>">
-                      <input type="submit" class="btn btn-styled btn-base-1 btn-circle" name="create">
-                    </div>
                   </div>
 
-                  <div class="col-md-6 ml-lg-auto">
-                    <div class="mt-4">
-                      <a href ="/account/login/">Already have an account? Sign in here.</a>
-                    </div>
+                  <div class="form-group">
+                    <?php if ($accountType == "Student") { //changes question for student or mentor
+                      echo '<label>Select the Region of the Island You Live In:</label>';
+                    }
+                    else {
+                      echo '<label>Select the Region of the Island You Work In:</label>';
+                    } ?>
+                    <select class="form-control selectpicker select2-hidden-accessible" data-minimum-results-for-search="Infinity" tabindex="-1" aria-hidden="true">
+                      <option value="North">North</option>
+                      <option value="Center">Centre</option>
+                      <option value="South">South</option>
+                    </select>
                   </div>
 
-                </div>
-              </form>
+                  <?php if ($accountType == "Student") { //changes question for student or mentor
+                    echo '
+                    <div class="form-group">
+                      <label class="control-label">Enter the Name of the School You Attend</label>
+                      <select class="form-control selectpicker select2-hidden-accessible" data-minimum-results-for-search="Infinity" tabindex="-1" aria-hidden="true">
+                        <option value="RGS">Ramsey Grammar School</option>
+                        <option value="CRHS">Castle Rushen High School</option>
+                        <option value="QEII">Queen Elizabeth II High School</option>
+                        <option value="BHS">Ballakermeen High School</option>
+                        <option value="SNHS">St Ninians High School</option>
+                        <option value="UCM">University College Isle of Man</option>
+                      </select>
+                    </div>';
+                  }
+                  else {
+                    echo '
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="form-group has-floating-label">
+                          <label class="control-label">Enter the Name of the Company You Work For</label>
+                          <input type="password" class="form-control form-control-lg" placeholder="">
+                          <span class="bar"></span>
+                        </div>
+                      </div>
+                    </div>';
+                  } ?>
+
+
+                  <div class="row cols-xs-space cols-sm-space cols-md-space align-items-center text-left">
+
+                    <div class="col-lg-3 col-md-4">
+                      <div class="mt-4">
+                        <input type="hidden" name="accountType" value="<?php echo $accountType?>">
+                        <input type="submit" class="btn btn-styled btn-base-1 btn-circle" name="create">
+                      </div>
+                    </div>
+
+                    <div class="col-md-6 ml-lg-auto">
+                      <div class="mt-4">
+                        <a href ="/account/login/">Already have an account? Sign in here.</a>
+                      </div>
+                    </div>
+
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-    </container>
+      </container>
 
-    <?php
-    include($root.'/includes/footer.php');
-    ?>
+      <?php
+      include($root.'/includes/footer.php');
+      ?>
+    }
+
   </main>
 
   <?php include($root.'/includes/scripts.php'); ?>
