d4d4c199c67af04ba1a97e6034c9f94269f3572f
Fergus <fergus.lole@outlook.com>
2019-03-04 15:31:28 +0000

    added outputs for posted values

--- account/create/details/index.php
+++ account/create/details/index.php
@@ -30,7 +30,17 @@ include($root.'/includes/connect.php');
 
       sqlsrv_query($conn, "INSERT INTO accounts (firstName, lastName, password, email, accountType, joinYear, region, workOrSchool) values ($firstName, $lastName, $password, $email, $accountType, $joinYear, $region, $workOrSchool) ");
       // echo '<script>window.location.href="/account/index.php";</script>';
-      echo "Executed Query";
+      echo "Executed Query:";
+      echo
+      "
+      <p>'.$firstName . " " . $lastName.'</p>
+      <p>'.$email.'</p>
+      <p>'.$password.'</p>
+      <p>'.$region.'</p>
+      <p>'.$workOrSchool.'</p>
+      <p>'.$joinYear.'</p>
+      <p>'.$accountType.'</p>
+      "
     }
     else {
       print "not running porcessing";
