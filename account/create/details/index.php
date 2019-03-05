1568c1ccc3e8ce02419df5039ae0c0c69b668bd6
Fergus <fergus.lole@outlook.com>
2019-03-05 15:22:54 +0000

    added check to see if account exist already

--- account/create/details/index.php
+++ account/create/details/index.php
@@ -29,14 +29,15 @@ include($root.'/includes/connect.php');
       $accountType = $_POST['accountType'];
 
       //checks for existing account with same Email
-
-      if (condition) {
-        // code...
+      $Query = sqlsrv_query($conn, "SELECT email FROM accounts WHERE email = $email", array(), array('Scrollable' => 'buffered'));
+      $count = sqlsrv_num_rows($Query);
+      if ($count > 0) {
+        print "<h5>This account aleady exists</h5>";
       }
       else {
         $insert = sqlsrv_query($conn, "INSERT INTO accounts (firstName, lastName, saltedPassword, email, accountType, joinYear, region, workOrSchool) values ($firstName, $lastName, $password, $email, $accountType, $joinYear, $region, $workOrSchool) ");
+        // echo '<script>window.location.href="/account/index.php";</script>';
       }
-      // echo '<script>window.location.href="/account/index.php";</script>';
     }
 
       $accountType = $_GET['type'];
