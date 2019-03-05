d9f876833d909a3772bb7ab77984501f7ee15c89
Fergus <fergus.lole@outlook.com>
2019-03-05 08:51:27 +0000

    started checking for existing account with same details

--- account/create/details/index.php
+++ account/create/details/index.php
@@ -28,7 +28,14 @@ include($root.'/includes/connect.php');
       $joinYear = (integer)date("Y");
       $accountType = $_POST['accountType'];
 
-      sqlsrv_query($conn, "INSERT INTO accounts (firstName, lastName, saltedPassword, email, accountType, joinYear, region, workOrSchool) values ($firstName, $lastName, $password, $email, $accountType, $joinYear, $region, $workOrSchool) ");
+      //checks for existing account with same Email
+
+      if (condition) {
+        // code...
+      }
+      else {
+        $insert = sqlsrv_query($conn, "INSERT INTO accounts (firstName, lastName, saltedPassword, email, accountType, joinYear, region, workOrSchool) values ($firstName, $lastName, $password, $email, $accountType, $joinYear, $region, $workOrSchool) ");
+      }
       // echo '<script>window.location.href="/account/index.php";</script>';
     }
 
