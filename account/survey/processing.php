a356cffab1cb7df2d27ad11aa80b20a640115e32
Fergus <fergus.lole@outlook.com>
2019-03-21 21:35:44 +0000

    unmessed qId declaration

--- account/survey/processing.php
+++ account/survey/processing.php
@@ -18,7 +18,7 @@ if (isset($_POST['SubmitCheck'])) {
   $Query = sqlsrv_query($conn, "SELECT qid FROM skillSurveyQs");
   //loops for each question
   while ($_POST = sqlsrv_fetch_array($Query)) {
-    $qid = $_POST['qid']; //takes question ID from SQL query
+    $qid = $_row['qid']; //takes question ID from SQL query
     $answer = $_POST['Question'.$qid.'']; //takes answer from post
     print $answer;
     $saveAnswer = sqlsrv_query($conn, "INSERT INTO skillSurveyAs (studentId, surveyNo, qId, dateCompleted, answer) values ($userId, $surveyNo, $qId, convert(date, getdate()), $answer)");
