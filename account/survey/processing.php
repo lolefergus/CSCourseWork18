6c6bc53591fac94e9c76bdf95ff65353f29e22e7
Fergus <fergus.lole@outlook.com>
2019-03-21 21:28:02 +0000

    chnaged query

--- account/survey/processing.php
+++ account/survey/processing.php
@@ -21,7 +21,7 @@ if (isset($_POST['SubmitCheck'])) {
     $qid = $row['qid']; //takes question ID from SQL query
     $answer = $_POST['Question'.$qid.'']; //takes answer from post
     print $answer;
-    $saveAnswer = sqlsrv_query($conn, "INSERT INTO skillSurveyAs (studentId, surveyNo, qId, dateCompleted, answer) values ($userId, $surveyNo, $qId, getdate(), $answer)");
+    $saveAnswer = sqlsrv_query($conn, "INSERT INTO skillSurveyAs (studentId, surveyNo, qId, dateCompleted, answer) values ($userId, $surveyNo, $qId, 12-11-2005, $answer)");
 
 
     //outputs $errors
