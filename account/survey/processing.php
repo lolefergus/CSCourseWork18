973e7209a2be19d12a0fbe7db4968af52a73e9d9
Fergus <fergus.lole@outlook.com>
2019-03-21 21:37:03 +0000

    replaced row where had been removed

--- account/survey/processing.php
+++ account/survey/processing.php
@@ -17,8 +17,8 @@ if (isset($_POST['SubmitCheck'])) {
   //gets list of all question IDs
   $Query = sqlsrv_query($conn, "SELECT qid FROM skillSurveyQs");
   //loops for each question
-  while ($_POST = sqlsrv_fetch_array($Query)) {
-    $qid = $_row['qid']; //takes question ID from SQL query
+  while ($row = sqlsrv_fetch_array($Query)) {
+    $qid = $row['qid']; //takes question ID from SQL query
     $answer = $_POST['Question'.$qid.'']; //takes answer from post
     print $answer;
     $saveAnswer = sqlsrv_query($conn, "INSERT INTO skillSurveyAs (studentId, surveyNo, qId, dateCompleted, answer) values ($userId, $surveyNo, $qId, convert(date, getdate()), $answer)");
