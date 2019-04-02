a94fb84e237e977a6e87b84682237b390ffdbc36
Fergus <fergus.lole@outlook.com>
2019-04-02 13:59:39 +0100

    better error output method

--- news/view/index.php
+++ news/view/index.php
@@ -16,7 +16,13 @@ $title = $row['title'];
 //sets up page
 include($root.'/includes/head.php');
 
-print sqlsrv_errors();
+if( ($errors = sqlsrv_errors() ) != null) {
+    foreach( $errors as $error ) {
+        echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
+        echo "code: ".$error[ 'code']."<br />";
+        echo "message: ".$error[ 'message']."<br />";
+    }
+}
 ?>
 <body>
 
