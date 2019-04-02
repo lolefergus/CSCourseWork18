a7fc963c58b0e7cd5d444ff7a3cda908adb1ce77
Fergus <fergus.lole@outlook.com>
2019-04-02 14:01:08 +0100

    ardg

--- news/view/index.php
+++ news/view/index.php
@@ -15,13 +15,14 @@ $title = $row['title'];
 
 //sets up page
 include($root.'/includes/head.php');
-
+if( $stmt === false ) {
 if( ($errors = sqlsrv_errors() ) != null) {
     foreach( $errors as $error ) {
         echo "SQLSTATE: ".$error[ 'SQLSTATE']."<br />";
         echo "code: ".$error[ 'code']."<br />";
         echo "message: ".$error[ 'message']."<br />";
     }
+}
 }
 ?>
 <body>
