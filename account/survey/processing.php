408bf303e169d1b689a9c511d379424cd6bc18b1
Fergus <fergus.lole@outlook.com>
2019-04-03 21:51:13 +0100

    refactored

--- account/survey/processing.php
+++ account/survey/processing.php
@@ -5,12 +5,14 @@ include($root.'/includes/connect.php');
 $userId = 15; //NEEDAS CHANGING TO USE SEESION
 
 //checks survey has been submited
-if (isset($_POST['SubmitCheck'])) {
+if (isset($_POST['SubmitCheck']))
+{
   $surveyNo = $_POST['surveyNo']; //sets survey number
   //gets list of all question IDs
   $Query = sqlsrv_query($conn, "SELECT qid FROM skillSurveyQs");
   //loops for each question
-  while ($row = sqlsrv_fetch_array($Query)) {
+  while ($row = sqlsrv_fetch_array($Query))
+  {
     $qId = $row['qid']; //takes question ID from SQL query
     $answer = $_POST['Question'.$qId.'']; //takes answer from post
     $saveAnswer = sqlsrv_query($conn, "INSERT INTO skillSurveyAs (studentId, surveyNo, qId, dateCompleted, answer) values ($userId, $surveyNo, $qId, convert(date, getdate()), $answer)");
