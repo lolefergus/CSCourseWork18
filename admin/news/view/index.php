304baba3c39e1e57772f62612184e7c085ed33de
Fergus <fergus.lole@outlook.com>
2019-04-04 10:54:35 +0100

    chnaged page names

--- admin/news/view/index.php
+++ admin/news/view/index.php
@@ -2,7 +2,7 @@
 <html>
 <?php
 $root = $_SERVER['DOCUMENT_ROOT'];
-$title = "N - Templates - Headers";
+$title = "Edit Article";
 // include($root.'/includes/session.php');
 include($root.'/includes/head.php');
 include($root.'/includes/connect.php');
