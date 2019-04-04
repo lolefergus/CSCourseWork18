6da144256ff47b1ae93c769c5f1008411f617902
Fergus <fergus.lole@outlook.com>
2019-04-04 16:43:09 +0100

    chnaghed logic

--- news/view/index.php
+++ news/view/index.php
@@ -9,86 +9,89 @@ $title = $row['title'];
 
 $isRows = sqlsrv_has_rows($Query);
 
-if ($isRows !=== true)
+if ($isRows === true)
 {
-  header('Location: /news/index.php');
+  ?>
+  <!DOCTYPE html>
+  <html>
+  <?php
+  $root = $_SERVER['DOCUMENT_ROOT'];
+
+  //conects to Database
+  include($root.'/includes/connect.php');
+
+
+  //sets up page
+  include($root.'/includes/head.php');
+
+
+  ?>
+  <body>
+
+    <main class="body-wrap">
+      <?php
+      include($root.'/includes/navbar.php');
+
+      //sets variables used to output articles
+        $title = $row['title'];
+        $body = $row['body'];
+        $image = $row['image'];
+        $author = $row['firstName'] . " " . $row['lastName'];
+        ?>
+
+        <!-- PAGE HEADER -->
+        <section class="text-center slice--offset-top parallax-section" style="background-image: url(<?php echo '/images/' .$image; ?>);">
+          <span class="mask mask-dark--style-2"></span>
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-8 my-3">
+                <div class="row py-5">
+                  <div class="col-12">
+                    <h1 class="heading heading-inverse heading-1 strong-400 text-normal">
+                      <?php echo $title; ?>
+                    </h1>
+                    <p class="heading heading-inverse heading-3 text-normal">
+                      <?php echo '-' . $author; ?>
+                    </p>
+                    <span class="clearfix"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <!-- body text -->
+        <?php
+        echo'
+
+        <section class="slice sct-color-1">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-8 my-3">
+                <div class="row py-5">
+                  <div class="col-12">
+                    <p>'.$body.'</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        ';
+
+
+      include($root.'/includes/footer.php');
+      ?>
+    </main>
+
+    <?php include($root.'/includes/scripts.php'); ?>
+
+  </body>
+  </html>
+  <?php
+}
+else {
+header('Location: /news/index.php');
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<?php
-$root = $_SERVER['DOCUMENT_ROOT'];
-
-//conects to Database
-include($root.'/includes/connect.php');
-
-
-//sets up page
-include($root.'/includes/head.php');
-
-
-?>
-<body>
-
-  <main class="body-wrap">
-    <?php
-    include($root.'/includes/navbar.php');
-
-    //sets variables used to output articles
-      $title = $row['title'];
-      $body = $row['body'];
-      $image = $row['image'];
-      $author = $row['firstName'] . " " . $row['lastName'];
-      ?>
-
-      <!-- PAGE HEADER -->
-      <section class="text-center slice--offset-top parallax-section" style="background-image: url(<?php echo '/images/' .$image; ?>);">
-        <span class="mask mask-dark--style-2"></span>
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-8 my-3">
-              <div class="row py-5">
-                <div class="col-12">
-                  <h1 class="heading heading-inverse heading-1 strong-400 text-normal">
-                    <?php echo $title; ?>
-                  </h1>
-                  <p class="heading heading-inverse heading-3 text-normal">
-                    <?php echo '-' . $author; ?>
-                  </p>
-                  <span class="clearfix"></span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <!-- body text -->
-      <?php
-      echo'
-
-      <section class="slice sct-color-1">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-8 my-3">
-              <div class="row py-5">
-                <div class="col-12">
-                  <p>'.$body.'</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      ';
-
-
-    include($root.'/includes/footer.php');
-    ?>
-  </main>
-
-  <?php include($root.'/includes/scripts.php'); ?>
-
-</body>
-</html>
