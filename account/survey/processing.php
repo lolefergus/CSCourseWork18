f859bf272fafa2ceab30eb13c0bbe334b4b85ac4
Fergus <fergus.lole@outlook.com>
2019-04-05 01:23:58 +0100

    reverted changes

--- account/survey/processing.php
+++ account/survey/processing.php
@@ -1,24 +1,26 @@
+<?php include($root.'/includes/session.php'); ?>
+<!DOCTYPE html>
+<html>
 <?php
-include($root.'/includes/session.php');
-include($root.'/includes/connect.php');
+    $root = $_SERVER['DOCUMENT_ROOT'];
+    $title = "Skill Survey"; //sets page title
+    include($root.'/includes/head.php');
+    include($root.'/includes/connect.php');
+    include($root.'/includes/navbar.php');
 
-$userId = 1; //NEEDAS CHANGING TO USE SEESION
-
+    $userId = 1;
 //checks survey has been submited
-if (isset($_POST['SubmitCheck']))
-{
+if (isset($_POST['SubmitCheck'])) {
   $surveyNo = $_POST['surveyNo']; //sets survey number
   //gets list of all question IDs
   $Query = sqlsrv_query($conn, "SELECT qid FROM skillSurveyQs");
   //loops for each question
-  while ($row = sqlsrv_fetch_array($Query))
-  {
+  while ($row = sqlsrv_fetch_array($Query)) {
     $qId = $row['qid']; //takes question ID from SQL query
     $answer = $_POST['Question'.$qId.'']; //takes answer from post
     $saveAnswer = sqlsrv_query($conn, "INSERT INTO skillSurveyAs (studentId, surveyNo, qId, dateCompleted, answer) values ($userId, $surveyNo, $qId, convert(date, getdate()), $answer)");
-    print "user id: " . $iserId . " QID: " $qId . " Answer: ". $answer;
   }
-  // header("Location:/account/");
+  header("Location:/account/");
 }
 else {
 print "An Error Has Occurred, Please Try Again";
